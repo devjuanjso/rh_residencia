9c5f13de62b24bdd434ea1a0ad0f1515380f30df
devjuanjso <juan.contato0101@gmail.com>
2026-01-05 10:16:17 -0400

    corrigindo candidaturas duplicadas

--- rh-backend/api/candidaturas/serializers.py
+++ rh-backend/api/candidaturas/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Candidatura
+from colaboradores.models import Colaborador
 
 
 class CandidaturaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidatura
         fields = "__all__"
+
+    def validate(self, attrs):
+        colaborador = attrs.get("colaborador")
+        vaga = attrs.get("vaga")
+
+        if Candidatura.objects.filter(
+            colaborador=colaborador,
+            vaga=vaga
+        ).exists():
+            raise serializers.ValidationError(
+                "Este colaborador já se candidatou a esta vaga."
+            )
+
+        return attrs
--- rh-backend/ia_service/geracao_texto/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GeracaoTextoConfig(AppConfig):
-    name = 'geracao_texto'
